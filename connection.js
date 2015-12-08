6d4a749dc1ffbbeaa631f3b790cdf140641188d0
abhinav <abhinav.saini@hippoinnovations.com>
2015-12-08 12:04:57 +0530

    8 Nov 15

--- connection.js
+++ connection.js
@@ -122,8 +122,51 @@ var addcomplaint= function(email,title,description,priority,now) {
 }
 
 
+var addsuggestion= function(email,title,description,now) {
+
+    var mongodb = require('mongodb');
+    var MongoClient = mongodb.MongoClient;
+
+    var url = 'mongodb://localhost:27017/HippoFeedo';
+
+    MongoClient.connect(url, function (err, db) {
+        if (err) {
+
+            console.log('Unable to connect to the mongoDB server. Error:', err);
+        }
+
+        else {
+            console.log('Connection established to', url);
+
+            // Get the documents collection
+            var collection = db.collection('suggestions');
+
+            //Create some suggestion
+            var data = {email:email,title:title,description:description,date:now};
+
+
+            /* var user2 = {name: 'modulus user', age: 22, roles: ['user']};
+             var user3 = {name: 'modulus super admin', age: 92, roles: ['super-admin', 'admin', 'moderator', 'user']};*/
+
+            // Insert some complaint
+            collection.insert(data, function (err, result) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log('Inserted %d documents into the "suggestions" collection. The documents inserted with "_id" are:', result.length, result);
+                }
+
+                db.close();
+            });
+
+        }
+    });
+}
+
+
 
 
 module.exports.add=add;
 module.exports.check=check;
 module.exports.addcomplaint=addcomplaint;
+module.exports.addsuggestion=addsuggestion;
